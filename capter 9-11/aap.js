c0a6d631fb661457fea9623203dde5f97b39d39e
Jannatsoomro01 <jannat.amir1992@gmail.com>
2024-09-06 18:59:39 +0500

    Add files via upload

---
+++ capter 9-11/aap.js
@@ -0,0 +1,96 @@
+/*chapter 9 to 11*/
+
+/* question no 1*/ 
+
+// var cityname = prompt("enter the cityname"); 
+//          if (cityname === "karachi")
+//          console.log("welcome to city of light")
+       
+
+         /*Q No 2*/ 
+
+        //  var gender = prompt("enter the gender"); 
+        //  if ( gender === "male")
+        //  console.log("Good morning sir")
+        // if (gender === "female")
+        //     console.log("Good morning Ma'am")
+       
+
+        /*Q No 3*/
+
+        // var trafficlight = prompt("enter the trafficlight"); 
+        //   if ( trafficlight === "red")
+        //  console.log("Stop must")
+        //  if (trafficlight === "yellow")
+        //    console.log("Ready to go")
+        // if (trafficlight === "green")
+        //     console.log("Move now")
+       
+
+
+        /* question no 4*/ 
+
+    //   var carfule = prompt("enter the carfule"); 
+    //    if (carfule === "litres")
+    //    console.log("Please refill the fuel in your car")
+
+
+ /* question no 5*/ 
+
+    //   var a = 4;
+    
+    // console.log(++a) 
+
+    //             5
+
+
+/* question no 6*/
+
+//                 var marksheet = prompt("enter the marksheet"); 
+//                 if (marksheet === "grater the or equal to 80")
+//                 console.log("A-one Excellent.")
+        
+//                 if (marksheet === "grater the or equal to 70")
+//                 console.log("A Good.")
+        
+//                 if (marksheet === "grater the or equal to 60")
+//                     console.log("B You need to improve.")
+            
+//                 if (marksheet === "less then 60")
+//                     console.log("Fail Sorry.")
+            
+// /* question no 8*/
+
+        //   var weather = prompt("enter the weather"); 
+        // if (weather === "3")
+        // console.log("if the number is divisable by 3")
+       
+    
+
+        /* question no 9*/
+
+    //       var weather = prompt("enter the weather"); 
+    //     if (weather === "40")
+    //     console.log("20.")
+       
+    //     if (weather === "10")
+    //     console.log("1.")
+       
+    // //     
+/* question no 10*/
+
+
+        // var tempercher = prompt("enter the tempercher"); 
+        // if (tempercher === "40")
+        // console.log("It is too hot outside.")
+
+     
+        // if (tempercher === "30")
+        //     console.log("The weather today is normal.")
+       
+        // if (tempercher === "20")
+        //     console.log("Today weather is cool.")
+       
+        // if (tempercher === "10")
+        //     console.log("Omg! Today's weather is so cool.")
+       
